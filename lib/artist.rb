e156bbadeda2b4445ed74ff1f8fd13ed084b02b4
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-08 20:08:50 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,9 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs :song_count
 
   def initialize(name)
     @name = name
     @songs = []
-  end
-
-  def songs
-    @songs
   end
 
   def add_song(song)
